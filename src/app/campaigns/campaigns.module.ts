ee3d1b9a9be89847729b601dda0bf08d82140553
Itokiana <itokianarajoelison@gmail.com>
2023-04-16 19:01:12 +0300

    📦 NEW: Prepare design for list campaign

--- src/app/campaigns/campaigns.module.ts
+++ src/app/campaigns/campaigns.module.ts
@@ -5,6 +5,18 @@ import { RouterModule } from '@angular/router';
 
 import { CampaignsRoutes } from './campaigns.routing';
 
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzBadgeModule } from 'ng-zorro-antd/badge';
+
 import { ListCampaignComponent } from './component/list-campaign/list-campaign.component';
 import { EditCampaignComponent } from './component/edit-campaign/edit-campaign.component';
 
@@ -20,6 +32,17 @@ import { EditCampaignComponent } from './component/edit-campaign/edit-campaign.c
     FormsModule,
     ReactiveFormsModule,
     RouterModule.forChild(CampaignsRoutes),
+    NzTypographyModule,
+    NzDatePickerModule,
+    NzFormModule,
+    NzCardModule,
+    NzButtonModule,
+    NzInputModule,
+    NzCheckboxModule,
+    NzSpaceModule,
+    NzSelectModule,
+    NzIconModule,
+    NzBadgeModule
   ]
 })
 export class CampaignsModule { }
